949c87d5c37467b415aa9b2297aa08ee7f1dad3a
manuel <mminuti3@gmail.com>
2020-11-06 15:42:16 +0100

    feat(message): timestamp

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: process.env.MESSAGE }));
-    //res.end(JSON.stringify({ message: process.env.MESSAGE, timestamp: new Date() }));
+    //res.end(JSON.stringify({ message: process.env.MESSAGE }));
+    res.end(JSON.stringify({ message: process.env.MESSAGE, timestamp: new Date() }));
 });
 
 server.listen(process.env.PORT);
